bfd113f7ed35909fc984b9701617da031c086c21
Brian MacKie-Mason <brimacki@gmail.com>
2018-06-06 17:48:46 -0500

    1. Added feature: Input family type; 2. Working on inputting federal bracket information

--- calculator.cpp
+++ calculator.cpp
@@ -2,11 +2,14 @@
 
 #include<iostream>
 #include<cmath>
+#include<fstream>
+#include<vector>
 
 double medicareRate = 0.0145;
 double socialSecurityRate = 0.062;
 double personalExemption = 0;
 double standardDeduction = 12000;
+double maximumIncome = 1.0e9;
 
 using namespace std;
 
@@ -15,8 +18,10 @@ int main(){
 	cout << "What is your gross income to the nearest cent?" << endl;
 	double grossIncome;
 	cin >> grossIncome;
-	cout << "This is your gross income: $";
-	int billions = grossIncome/1.0e9;
+	cout << "This is your gross income: $" << grossIncome << endl;
+
+	//The following function is a cool function to write eventually.
+	/*int billions = grossIncome/1.0e9;
 	if(billions > 0){
 		cout << billions << ",";
 	}
@@ -35,17 +40,60 @@ int main(){
 	double cents = (grossIncome - billions*1.0e9 - millions*1.0e6 - thousands*1.0e3 - ones)*100;
 	if(cents > 0){
 		cout << cents << "." << endl;
-	}
-
-	double federalTaxable = grossIncome - personalExemption - standardDeduction;
-
-	cout << "This is your federal taxable income: " << federalTaxable << endl;
+	}*/
 
 	double medicareTaxes = medicareRate*grossIncome;
+	cout << "These are your medicare taxes: $" << medicareTaxes << endl;
 	double socialSecurityTaxes = socialSecurityRate*grossIncome;
 
-	cout << "These are your medicare taxes: " << medicareTaxes << endl;
-	cout << "These are your social security taxes: " << socialSecurityTaxes << endl;
+	
+	cout << "These are your social security taxes: $" << socialSecurityTaxes << endl;
+
+	double federalTaxable = grossIncome - personalExemption - standardDeduction;
+	cout << "This is your federal taxable income: $" << federalTaxable << endl;
+
+	//Input federal brackets
+	vector <string> familyTypes(4);
+	familyTypes.at(0) = "Single";
+	familyTypes.at(1) = "MFS";
+	familyTypes.at(2) = "MFJ";
+	familyTypes.at(3) = "HoH";
+	cout << "Under what family classification are you filing? You must enter (case sensitive): 'Single', 'MFS', 'MFJ', or 'HoH'." << endl;
+	string familyType;
+	cin >> familyType;
+	int familyID = -1;
+	if(familyType == "Single"){
+		familyID = 1;
+	}
+	else if(familyType == "MFS"){
+		familyID = 2;
+	}
+	else if(familyType == "MFJ"){
+		familyID = 3;
+	}
+	else if(familyType == "HoH"){
+		familyID = 4;
+	}
+	else{
+		cout << "You have failed to enter the correct family status identifier. Exiting now..." << endl;
+	}
+
+	ifstream federalBrackets;
+	federalBrackets.open("taxBrackets/federal2018.txt");
+	if(!federalBrackets.is_open()){
+		cout << "Could not open bracket definitions! Exiting now..." << endl;
+	}
+
+	int numBrackets;
+	for(int i = 0; i < familyID; ++i){
+		if(i == familyID){
+			federalBrackets >> numBrackets;
+		}
+		else{
+		}
+	}
+
+	//Calculate federal income tax
 
 	return 0;
 }
